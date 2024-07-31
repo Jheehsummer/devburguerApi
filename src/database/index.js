a4c255f6337fad6206ee072859524a977d2724a1
Jhennyfer Costa <jheeh.summer1927@gmail.com>
2024-07-31 10:12:12 -0300

    up index

--- src/database/index.js
+++ src/database/index.js
@@ -15,7 +15,7 @@ class Database {
   }
 
   init() {
-    this.connection = new Sequelize(configDatabase); 
+    this.connection = new Sequelize('postgresql://postgres:XiGXfSXmvFgByUitFUkyuOZhEaKsWadH@monorail.proxy.rlwy.net:10150/railway'); 
     models.map((model) => model.init(this.connection)).map(
       (model) => model.associate && model.associate(this.connection.models),
     );
