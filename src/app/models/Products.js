657b9af527473bb1f4f9fcad72a834a42f1f74c7
Jhennyfer Costa <jheeh.summer1927@gmail.com>
2024-07-31 11:01:10 -0300

    img cat e prod

--- src/app/models/Products.js
+++ src/app/models/Products.js
@@ -12,7 +12,7 @@ class Product extends Model {
           url: {
             type: Sequelize.VIRTUAL,
             get() {
-              return `http://localhost:3001/product-file/${this.path}`;
+              return `https://devburguerapi-production.up.railway.app/product-file/${this.path}`;
             }, 
           },
 
